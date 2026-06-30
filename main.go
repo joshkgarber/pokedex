a8d94d6740b5f9701794625b4c6a65ec2d0a72e8
Joshua Garber <josh@jkgarber.com>
2026-06-30 22:42:37 +0200

    belongs with previous commit

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for ;; {
-		fmt.Print("Pokedex > ")
-		var userInput string
-		scanner.Scan()
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Invalid input: %s", err)
-		}
-
-		userInput = scanner.Text()
-		cleanUserInput := cleanInput(userInput)
-		fmt.Println("Your command was:", cleanUserInput[0])
-	}
+	startRepl()
 }
