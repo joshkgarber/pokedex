c7278a203cc879894b792a342e81c210370f081a
Joshua Garber <josh@jkgarber.com>
2026-06-30 22:34:09 +0200

    add first repl structure

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for ;; {
+		fmt.Print("Pokedex > ")
+		var userInput string
+		scanner.Scan()
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Invalid input: %s", err)
+		}
+
+		userInput = scanner.Text()
+		cleanUserInput := cleanInput(userInput)
+		fmt.Println("Your command was:", cleanUserInput[0])
+	}
 }
--- repl.go
+++ repl.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	// Examples:
-	// "hello world" -> ["hello", "world"]
-	// "Charmander Bulbasur PIKACHU" -> ["charmander", "bulbasur", "pikachu"]
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
 
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Invalid input: %s", err)
+		}
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Println("Your command was:", commandName)
+	}
+}
+
+func cleanInput(text string) []string {
 	loweredInputs := strings.ToLower(text)
 	splitInputs := strings.Fields(loweredInputs)
-
 	return splitInputs
 }
 
